e3eb40944b1c80a3316209b116cc51ffd3933f03
22r01a0534 <22r01a0534@cmtithyderabad.edu.in>
2025-03-18 23:27:47 +0530

    commit

--- app/[...not_found]/layout.tsx
+++ app/[...not_found]/layout.tsx
@@ -1,9 +1,5 @@
 // import type { Viewport } from 'next'
 
-export const viewport = {
-    themeColor: "#ffffff", // Example color
-    colorScheme: "light dark", // Updated to a single value for clarity
-};
 export const metadata = {
     title: 'Portfolio - Sathwika', // Capitalized 'Sathwika' for consistency
     description: "Welcome to my portfolio website! I'm currently pursuing my B.Tech and am passionate about connecting with like-minded individuals. Let's discuss how we can collaborate on exciting projects!",
