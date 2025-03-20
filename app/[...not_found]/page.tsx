fbf40a027603cda7cffefc7d2c46934338ba6864
22r01a0534 <22r01a0534@cmtithyderabad.edu.in>
2025-03-20 19:55:59 +0530

    commit

--- app/[...not_found]/page.tsx
+++ app/[...not_found]/page.tsx
@@ -1,13 +1,5 @@
-export default function NotFoundPage() {
-    return (
-        <div>
-            <h1>404 - Page Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
-        </div>
-    );
-}
+import { notFound } from "next/navigation";
 
-// Fix: Add this function for static export
-export function generateStaticParams() {
-    return [{ not_found: [] }]; // Empty array to handle catch-all route
+export default function NotFoundCatchAll() {
+    notFound();
 }
--- next.config.js
+++ next.config.js
@@ -5,34 +5,30 @@ const outputDir = process.env.BRANCH === 'dev' ? 'dev' : '.next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/mamindlasathwika.io",
-  assetPrefix: "/mamindlasathwika.io/",
-  distDir: outputDir,
-  output: 'export',
-  trailingSlash: true,
-  reactStrictMode: true,
-  compiler: {
-    reactRemoveProperties: isProduction,
-    removeConsole: isProduction,
-    styledComponents: {
-      displayName: !isProduction,
-      minify: isProduction,
-      pure: true,
+    distDir: outputDir,
+    compiler: {
+        reactRemoveProperties: isProduction,
+        removeConsole: isProduction,
+        styledComponents: {
+            displayName: !isProduction,
+            minify: isProduction,
+            pure: true,
+        },
     },
-  },
-  experimental: {
-    // appDir: true
-  },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        pathname: '/22r01a0534/mamindlasathwika.io/main/public/images/**',
-      },
-    ],
-  },
+    devIndicators: {
+        position: "top-right",
+    },
+    experimental: {},
+    productionBrowserSourceMaps: isProduction,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "raw.githubusercontent.com",
+            },
+        ],
+    },
+    pageExtensions: ["ts", "tsx", "js", "jsx", "mdx"],
 };
 
 module.exports = nextConfig;
