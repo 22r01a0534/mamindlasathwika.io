ef4b8d9cb4e3cc2468901734d0bdc05ae373fb6a
22r01a0534 <22r01a0534@cmtithyderabad.edu.in>
2025-03-20 20:15:06 +0530

    commit

--- app/components/work/ProjectCard.tsx
+++ app/components/work/ProjectCard.tsx
@@ -1,6 +1,6 @@
 import { ProjectProps } from "./projectDetails";
 import Link from "next/link";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import AnimatedTitle from "../../animations/AnimatedTitle";
 import AnimatedBody from "../../animations/AnimatedBody";
 import { motion } from "framer-motion";
