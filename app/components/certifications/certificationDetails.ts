1841f151074de1d9b093d8c13f7a41ec2e63f8ef
22r01a0534 <22r01a0534@cmtithyderabad.edu.in>
2025-03-20 21:46:52 +0530

    commit

--- app/components/certifications/certificationDetails.ts
+++ app/components/certifications/certificationDetails.ts
@@ -9,7 +9,7 @@ export const certifications: CertificationProps[] = [
     {
         id: 0,
         name: "Infosys Springboard basics of Python Certified",
-        image: "../certifications/infosyscertification.JPG",
+        image: "./certifications/infosyscertification.JPG",
         link: "https://drive.google.com/file/d/1fvj50nsAzAPbRanJC46stfEVYodhMpxd/view?usp=sharing"
     },
 ];
