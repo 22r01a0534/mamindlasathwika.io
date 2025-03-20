13985968182dd5ceac427e6a7c34f4679113b9fe
22r01a0534 <22r01a0534@cmtithyderabad.edu.in>
2025-03-20 22:09:02 +0530

    commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,6 +13,7 @@ export const metadata: Metadata = {
     icons: {
         icon: "./your/favicon.ico", // Correct favicon path
     },
+    viewport: "width=device-width, initial-scale=1.0",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
